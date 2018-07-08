d9f0b8afce73544bd29eb76d1a2a4d34ab8c1533
llllllllllar <659149178@qq.com>
2018-07-08 15:48:54 +0800

    minesweeper

--- apps/minesweeper/minesweeper.js
+++ apps/minesweeper/minesweeper.js
@@ -83,101 +83,142 @@ var markedSquare = function(array) {
 var templatePuzzle = function(n) {
     var a = randomSquare09(n)
     var s = markedSquare(a)
+    log('扫雷答案', s)
     for (var i = 0; i < n; i++) {
         var l = s[i]
-        var t1 = `<div id="id-mines-row-${i}" class="mines-row" data-x=${i}></div>`
+        var t1 = `<div id="id-mines-row-${i}" class="mines-row" data-row=${i}></div>`
         var e1 = e('#id-game-box')
         appendHtml(e1, t1)
         for (var j = 0; j < n; j++) {
             var m = l[j]
-            // if (m == 9) {
-            //     var t2 = `
-            //         <div id="id-mines-col-${j}" class="mines-col img-boom mask" data-col=${j}>
-            //             <span>&nbsp;</span>
-            //             <img src="minesweeper_pics/flag.svg" alt="flag" class="img-flag hide">
-            //         </div>
-            //     `
-            // } else if (m == 0) {
-            //     var t2 = `
-            //         <div id="id-mines-col-${j}" class="mines-col mask" data-col=${j}>
-            //             <span>&nbsp;</span>
-            //             <img src="minesweeper_pics/flag.svg" alt="flag" class="img-flag hide">
-            //         </div>
-            //     `
-            // } else {
-            //     var t2 = `
-            //         <div id="id-mines-col-${j}" class="mines-col mask" data-col=${j}>
-            //             <span>${m}</span>
-            //             <img src="minesweeper_pics/flag.svg" alt="flag" class="img-flag hide">
-            //         </div>
-            //     `
-            // }
-            var t2 = `
-                <div id="id-mines-col-${j}" class="mines-col mask" data-y=${j}>
-                    <span>${m}</span>
-                    <img src="minesweeper_pics/flag.svg" alt="flag" class="img-flag hide">
-                </div>
-            `
+            if (m == 9) {
+                var t2 = `
+                    <div id="id-mines-col-${j}" class="mines-col mines-boom mask" data-col=${j}>
+                        <div class="mines-cell" data-num=${m}>&nbsp;</div>
+                        <div class="mines-flag"><img src="minesweeper_pics/flag.svg" alt="flag" class="img-flag hide"></div>
+                    </div>
+                `
+            } else if (m == 0) {
+                var t2 = `
+                    <div id="id-mines-col-${j}" class="mines-col mask" data-col=${j}>
+                        <div class="mines-cell" data-num=${m}>&nbsp;</div>
+                        <div class="mines-flag"><img src="minesweeper_pics/flag.svg" alt="flag" class="img-flag hide"></div>
+                    </div>
+                `
+            } else {
+                var t2 = `
+                    <div id="id-mines-col-${j}" class="mines-col mask" data-col=${j}>
+                        <div class="mines-cell" data-num=${m}>${m}</div>
+                        <div class="mines-flag"><img src="minesweeper_pics/flag.svg" alt="flag" class="img-flag hide"></div>
+                    </div>
+                `
+            }
             var e2 = e(`#id-mines-row-${i}`)
             appendHtml(e2, t2)
-
-            // hoverPuzzle(x, y)
         }
     }
 }
 
-// var puzzle = (n) {
-//     var a = randomSquare09(n)
-//     var s = markedSquare(a)
-//     for (var i = 0; i < n; i++) {
-//         var l = s[i]
-//         for (var i = 0; i < n; i++) {
-//             var m = l[j]
-//             if (m == 9) {
-//                 var e1 = e('mines-col')
+var totalMines = function() {
+    var ms = []
+    var mcell = es('.mines-cell')
+    for (var i = 0; i < mcell.length; i++) {
+        if (mcell[i].dataset.num == '9') {
+            ms.push(mcell[i])
+        }
+    }
+    return ms.length
+}
+
+var totalFlags = function() {
+    var fs = []
+    var flags = es('.img-flag')
+    for (var i = 0; i < flags.length; i++) {
+        if (!flags[i].classList.contains('hide')) {
+            fs.push(flags[i])
+        }
+    }
+    return fs.length
+}
+
+var toggleHide = function(element) {
+    if (element.classList.contains('hide')) {
+        element.classList.remove('hide')
+        element.classList.toggle('show')
+    } else {
+        element.classList.remove('show')
+        element.classList.toggle('hide')
+    }
+}
+
+var bindContextMenu = function() {
+    var box = e('#id-game-box')
+    bindEvent(box, 'contextmenu', function(event) {
+        event.preventDefault()
+        var self = event.target
+        if (self.classList.contains('mines-col') || self.classList.contains('mines-cell') || self.classList.contains('mines-flag') || self.classList.contains('img-flag')) {
+            var col = self.closest('.mines-col')
+            var img = col.querySelector('.img-flag')
+            toggleHide(img)
+            totalFlags()
+        }
+    })
+}
+
+// var showBlankAround = function() {
 //
-//             }
-//         }
-//     }
 // }
 
+var bindClick = function() {
+    var box = e('#id-game-box')
+    bindEvent(box, 'click', function(event) {
+        var self = event.target
+        if (self.classList.contains('mines-col') || self.classList.contains('mines-cell') || self.classList.contains('mines-flag') || self.classList.contains('img-flag')) {
+            log('click start')
+            var col = self.closest('.mines-col')
+            var cell = col.querySelector('.mines-cell')
+            var flag = col.querySelector('.img-flag')
+            if (cell.dataset.num == '9') {
+                var booms = es('.mines-boom')
+                for (var i = 0; i < booms.length; i++) {
+                    var boomCell = booms[i].querySelector('.mines-cell')
+                    booms[i].classList.remove('mask')
+                    booms[i].style.backgroundImage = 'url(minesweeper_pics/boom.svg)'
+                    boomCell.style.opacity = '1'
+                }
+                // alert('boom, you lose')
+            } else if (cell.dataset.num == '0') {
+                var row = self.closest('.mines-row')
+                col.classList.remove('mask')
+                cell.style.opacity = '1'
+                // showBlankAround()
+                
+            } else if (flag.classList.contains('show')) {
 
-var hoverPuzzle = function() {
-    // bindAll('.mines-col', 'mouseover', () {
-    //     removeClassAll('mask')
-    // })
-    // bindAll('.mines-col', 'mouseout', () {
-    //     addClassAll('mask')
-    // })
-    // for (var i = 0; i < array.length; i++) {
-    //     array[i]
-    // }
-
+            } else {
+                col.classList.remove('mask')
+                cell.style.opacity = '1'
+            }
+            // else if (!flag.classList.contains('hide')) {
+            //
+            // }
+        }
+    })
 }
+
 var initPuzzles = function() {
-    templatePuzzle(12)
-    // var btn = e(`#id-game-level-easy`)
-    // bindEvent(btn, 'click', () {
-    //     var gameBox = e('#id-game-box')
-    //     if () {
-    //         templatePuzzle(9)
-    //     } else {
-    //
-    //     }
-    // })
-}
-
-var gameLevel = function(level) {
-
+    templatePuzzle(16)
 }
 
 var bindEvents = function() {
-    hoverPuzzle()
+    bindContextMenu()
+    bindClick()
 }
 
 var __main = function() {
     height()
     initPuzzles()
     bindEvents()
+    totalMines()
 }
 __main()
