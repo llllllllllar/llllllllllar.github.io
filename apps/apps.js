16f8600e68d8e214845a0e3bff4d034d805b3445
llllllllllar <659149178@qq.com>
2018-07-11 00:43:42 +0800

    delete minesweeper temporarily

--- apps/apps.js
+++ apps/apps.js
@@ -10,8 +10,8 @@ var templateApp = (appCn, appEn) => {
 }
 var insertApp = () => {
     var linkBox = e('.link-box')
-    var a1 = ['扫雷', '音乐', '广州三天天气', '待办事项']
-    var a2 = ['minesweeper', 'music', 'weather', 'todo']
+    var a1 = ['音乐', '广州三天天气', '待办事项']
+    var a2 = ['music', 'weather', 'todo']
     for (var i = 0; i < a1.length; i++) {
         var html = templateApp(a1[i], a2[i])
         appendHtml(linkBox, html)
