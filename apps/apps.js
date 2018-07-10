484d6b332786c88c4ea3f0ceb13a80f5db180ecd
llllllllllar <659149178@qq.com>
2018-07-10 21:45:30 +0800

    delete the blog temporarily

---
+++ apps/apps.js
@@ -0,0 +1,25 @@
+var height = () => {
+    var e1 = e('.container')
+    elementHeight(e1)
+}
+var templateApp = (appCn, appEn) => {
+    var t = `
+        <div class="link-text fengxing-float-left"><a href="/apps/${appEn}/${appEn}.html">${appCn}<br/>${appEn}</a></div>
+    `
+    return t
+}
+var insertApp = () => {
+    var linkBox = e('.link-box')
+    var a1 = ['扫雷', '音乐', '广州三天天气', '待办事项']
+    var a2 = ['minesweeper', 'music', 'weather', 'todo']
+    for (var i = 0; i < a1.length; i++) {
+        var html = templateApp(a1[i], a2[i])
+        appendHtml(linkBox, html)
+    }
+}
+
+var __main = () => {
+    height()
+    insertApp()
+}
+__main()
